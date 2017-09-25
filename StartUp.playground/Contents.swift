546f6c129e63e6b808b4df188c537c9bdbe284d8
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-09-25 12:45:57 -0600

    Played with more variable types and printed a message.

--- StartUp.playground/Contents.swift
+++ StartUp.playground/Contents.swift
@@ -6,8 +6,21 @@ var thisName :String = "meow"
 
 var welcomeMessage: String
 
-welcomeMessage = "Hello world"
+welcomeMessage = "What up dawg?"
 
 let meaningOfLife = "42"
+
+var name = "Mr teacher man"
+
+print("hello,  \(name)!")
+
+//prints "Hello, Mr teacher man!"
+
+print(welcomeMessage)
+
+let coolBackRow = "Connor, Noah, Will, Jian, Aron"
+print("You have a real cool back row, these peeps are")
+print(coolBackRow)
+
 
 
