5d1832509bb259dbcacdb8e1d95828a3e9e34cc3
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-09-25 13:50:28 -0600

    added a character counter and what not

--- StartUp.playground/Contents.swift
+++ StartUp.playground/Contents.swift
@@ -3,6 +3,8 @@ import UIKit
 var myName :String
 var otherName = "Words"
 var thisName :String = "meow"
+print(thisName.count)
+print(thisName.characters.count)
 
 var welcomeMessage: String
 
@@ -21,6 +23,14 @@ print(welcomeMessage)
 let coolBackRow = "Connor, Noah, Will, Jian, Aron"
 print("You have a real cool back row, these peeps are")
 print(coolBackRow)
+
+
+var age = 2315
+var speed = 654.324536
+var rate = age/Int (speed)
+
+1 + 2
+5 + 3
 
 
 
