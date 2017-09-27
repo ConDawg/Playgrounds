2ab444482ff1f4e8e4b6e9159efc7818972d9c3d
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-09-27 12:53:41 -0600

    Created an "if else" and "while" loop

--- Methods and Ifs.playground/Contents.swift
+++ Methods and Ifs.playground/Contents.swift
@@ -17,3 +17,36 @@ aBitLessSimple(name: "Rick!!!")
 let words = "Marshall Mathers"
 aBitLessSimple(name: words)
 
+public func namingExample(outerName innerName : String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Connor")
+
+public func counting() -> Int
+{
+    return (7 * 248)
+}
+print("I am not \(counting()) years old")
+
+if(counting() < 10)
+{
+    print("math works in swift too")
+}
+else
+{
+    print("not very likely right now")
+}
+var test = 0
+
+while (test < counting())
+{
+    print("hahahahahaUrDumbhahahahahaha")
+    test += 1
+}
+
+print("yay!")
+
+
+
+
