07e5cdc946ce92042ab9a46676fc7540e6493559
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-10-02 12:50:24 -0600

    Demo with some sample classes and methods

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -12,4 +12,44 @@ public class SimpleClass
     {
         name = "my name is....."
     }
+    
+    public func getName() -> String
+    {
+        return name
+    }
 }
+var sample = SimpleClass()
+print(sample.getName())
+
+public class OtherClass
+{
+    private var favoriteNumber :Int
+    private var favoriteWord :String
+    
+    public init()
+    {
+        favoriteNumber = Int()
+        favoriteWord = String()
+    }
+    
+    public init(favoriteNumber: Int, favoriteWord :String)
+    {
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeInternalState() -> Void
+    {
+        self.favoriteNumber *= 1908
+        self.favoriteWord += " and more and more"
+    }
+}
+//Swift version
+var secondClass = OtherClass.init()
+//Java would be OtherClass sample = new OtherClass();
+//it is also not = OtherClass.init()
+var thirdSample = OtherClass(favoriteNumber: 238957, favoriteWord :"Blimey")
+secondClass.changeInternalState()
+thirdSample.changeInternalState()
+thirdSample.changeInternalState()
+
